0bac2681c89a8916bc4150d190b62531d22e8eda
Eric Butler <ericbutler555@gmail.com>
2020-09-02 12:48:08 -0400

    Remove optional ViewChild property, fix recipe detail dropdown

--- src/app/shopping-list/edit/edit.component.ts
+++ src/app/shopping-list/edit/edit.component.ts
@@ -7,8 +7,8 @@ import { Ingredient } from 'src/app/shared/ingredient';
   styleUrls: ['./edit.component.scss']
 })
 export class ShoppingListEditComponent implements OnInit {
-  @ViewChild('nameInput', {static: false}) nameInputRef: ElementRef;
-  @ViewChild('amountInput', {static: false}) amountInputRef: ElementRef;
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
 
   @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
